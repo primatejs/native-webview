06edcbda7470b413a6b48f0af7e9daa40ccaa75a
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-09 19:59:06 +0200

    switch to js

--- src/ffi.js
+++ src/ffi.js
@@ -2,76 +2,77 @@ import { dlopen, FFIType, ptr } from "bun:ffi";
 import { Webview } from "./webview";
 import bin from "../build/libwebview.bin";
 
-export function encodeCString(value: string) {
-    return ptr(new TextEncoder().encode(value + "\0"));
-}
+export const encodeCString = value =>
+  ptr(new TextEncoder().encode(`${value }\0`));
 
-export const instances: Webview[] = [];
+export const instances = [];
 
 /**
  * Unload the library and destroy all webview instances. Should only be run
  * once all windows are closed.
  */
-export function unload() {
-    for (const instance of instances) instance.destroy();
-    lib.close();
-}
+export const unload = () => {
+  for (const instance of instances) {
+    instance.destroy();
+  }
+  lib.close();
+};
 
 export const lib = dlopen(process.env.WEBVIEW_PATH ?? Bun.file(bin), {
     webview_create: {
         args: [FFIType.i32, FFIType.ptr],
-        returns: FFIType.ptr
+        returns: FFIType.ptr,
     },
     webview_destroy: {
         args: [FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_run: {
         args: [FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_terminate: {
         args: [FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_get_window: {
         args: [FFIType.ptr],
-        returns: FFIType.ptr
+        returns: FFIType.ptr,
     },
     webview_set_title: {
         args: [FFIType.ptr, FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_set_size: {
         args: [FFIType.ptr, FFIType.i32, FFIType.i32, FFIType.i32],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_navigate: {
         args: [FFIType.ptr, FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_set_html: {
         args: [FFIType.ptr, FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_init: {
         args: [FFIType.ptr, FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_eval: {
         args: [FFIType.ptr, FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_bind: {
         args: [FFIType.ptr, FFIType.ptr, FFIType.function, FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_unbind: {
         args: [FFIType.ptr, FFIType.ptr],
-        returns: FFIType.void
+        returns: FFIType.void,
     },
     webview_return: {
         args: [FFIType.ptr, FFIType.ptr, FFIType.i32, FFIType.ptr],
-        returns: FFIType.void
-    }
+        returns: FFIType.void,
+    },
 });
---
+++ src/webview.js
@@ -0,0 +1,122 @@
+import { CString, FFIType, JSCallback } from "bun:ffi";
+import { encodeCString, instances, lib } from "./ffi";
+
+export class Webview {
+  #handle = null;
+  #callbacks = new Map();
+
+  get unsafeHandle() {
+    return this.#handle;
+  }
+
+  get unsafeWindowHandle() {
+    return lib.symbols.webview_get_window(this.#handle);
+  }
+
+  set size({ width, height, hint }) {
+    lib.symbols.webview_set_size(this.#handle, width, height, hint);
+  }
+
+  set title(title) {
+    lib.symbols.webview_set_title(this.#handle, encodeCString(title));
+  }
+
+  constructor(
+    debugOrHandle = false,
+    size = { width: 1024, height: 768 },
+    window = null,
+  ) {
+    this.#handle = typeof debugOrHandle === "bigint" || typeof debugOrHandle === "number"
+      ? debugOrHandle
+      : lib.symbols.webview_create(Number(debugOrHandle), window);
+    if (size !== undefined) {
+      this.size = size;
+    }
+    instances.push(this);
+  }
+
+  destroy() {
+    for (const callback of this.#callbacks.keys()) {
+      this.unbind(callback);
+    }
+    lib.symbols.webview_terminate(this.#handle);
+    lib.symbols.webview_destroy(this.#handle);
+    this.#handle = null;
+  }
+
+  navigate(url) {
+    lib.symbols.webview_navigate(this.#handle, encodeCString(url));
+  }
+
+  setHTML(html) {
+    lib.symbols.webview_set_html(this.#handle, encodeCString(html));
+  }
+
+  run() {
+    lib.symbols.webview_run(this.#handle);
+    this.destroy();
+  }
+
+  bindRaw(name, callback, arg) {
+    const callbackResource = new JSCallback((seqPtr, reqPtr, arg) => {
+      const seq = seqPtr ? new CString(seqPtr) : "";
+      const req = reqPtr ? new CString(reqPtr) : "";
+      callback(seq, req, arg);
+    }, {
+      args: [FFIType.pointer, FFIType.pointer, FFIType.pointer],
+      returns: FFIType.void,
+    });
+    this.#callbacks.set(name, callbackResource);
+    lib.symbols.webview_bind(
+      this.#handle,
+      encodeCString(name),
+      callbackResource.ptr,
+      arg,
+    );
+  }
+
+  bind(name, callback) {
+    this.bindRaw(name, (seq, req) => {
+      const args = JSON.parse(req);
+      let result;
+      let success;
+      try {
+        result = callback(...args);
+        success = true;
+      } catch (err) {
+        result = err;
+        success = false;
+      }
+      if (result instanceof Promise) {
+        result.then((result) =>
+          this.return(seq, success ? 0 : 1, JSON.stringify(result)),
+        );
+      } else {
+        this.return(seq, success ? 0 : 1, JSON.stringify(result));
+      }
+    });
+  }
+
+  unbind(name) {
+    lib.symbols.webview_unbind(this.#handle, encodeCString(name));
+    this.#callbacks.get(name)?.close();
+    this.#callbacks.delete(name);
+  }
+
+  return(seq, status, result) {
+    lib.symbols.webview_return(
+      this.#handle,
+      encodeCString(seq),
+      status,
+      encodeCString(result),
+    );
+  }
+
+  eval(source) {
+    lib.symbols.webview_eval(this.#handle, encodeCString(source));
+  }
+
+  init(source) {
+    lib.symbols.webview_init(this.#handle, encodeCString(source));
+  }
+}
